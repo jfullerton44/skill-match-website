96349c4f7dc4008929a75246f5de98b27b824426
Jacob Fullerton <fullertonj44@gmail.com>
2019-02-11 16:06:16 -0500

    Add skillMatch App and simple models

---
+++ skillMatch/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Student(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    classes = models.ManyToManyField('Class',blank=True)
+    skills = models.ManyToManyField('Skill', blank=True)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Class(models.Model):
+    prefix = models.CharField(max_length=5)
+    course_number = models.CharField(max_length=5)
+    professor = models.CharField(max_length=50)
+    semester = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
