fcb067eb5f2ab26d907f3df3e00b8470119717ba
Jacob Fullerton <fullertonj44@gmail.com>
2019-02-11 16:29:45 -0500

    add admin for class and skill

--- skillMatch/admin.py
+++ skillMatch/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
 
-from .models import Student
+from .models import Student, Class, Skill
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name','sex','bio']
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ['prefix',"course_number",'professor','semester']
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+
