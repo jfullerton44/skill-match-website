2d4b4ecd8b3f1318b0d038342488ed6952b459d7
Jacob Fullerton <fullertonj44@gmail.com>
2019-02-18 16:12:42 -0500

    Make computing id required to be unique

--- skillMatch/models.py
+++ skillMatch/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Student(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
     name = models.CharField(max_length=100)
-    computing_id = models.CharField(max_length=6)
+    computing_id = models.CharField(max_length=6, unique= True)
     bio = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     classes = models.ManyToManyField('Class',blank=True)
@@ -11,7 +11,7 @@ class Student(models.Model):
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
