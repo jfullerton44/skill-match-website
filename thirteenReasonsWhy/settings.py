90dc4ce0fc3628e8c43a97c3c435299d5eba5326
Jacob Fullerton <fullertonj44@gmail.com>
2019-04-15 16:26:17 -0400

    Add allowed hosts

--- thirteenReasonsWhy/settings.py
+++ thirteenReasonsWhy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '8s^o^-*v3ue!hnq_ks_#uu0uhff3%&ymoax=0y8pr+23)^%rj4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
