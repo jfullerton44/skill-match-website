99df9afbdfbf2866d1eed393db9fb61ba83bf42a
Jeffrey Rhoads <jtr5ee@virginia.edu>
2019-04-22 13:35:48 -0400

    Adding the addfriend/remove button functionality to other places
    Adding the addfriend/remove button functionality to other places

--- skillMatch/urls.py
+++ skillMatch/urls.py
@@ -6,7 +6,8 @@ app_name = 'skillMatch'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('addfriend/<slug:student_id>/', views.addfriend, name="addfriend"),
+    path('addfriend/<slug:student_id>/', views.addfriend, name='addfriend'),
+    path('removefriend/<slug:student_id>/', views.removefriend, name='removefriend'),
     path('createProfile/', views.ProfileCreateView.as_view(), name='profile_create'),
     path('students/<slug:user_id>/', views.studentProfileView, name='profile'),
     path('students/<slug:user>/edit/', views.StudentUpdateView.as_view(), name='student_edit'),
@@ -20,6 +21,5 @@ urlpatterns = [
     path('addClass/', views.classListView.as_view(), name='class_list'),
     path('addOneClass/<slug:user_id>/<slug:class_id>', views.addclass, name ='add_class'),
     path('class/<slug:course_pk>/', views.classDetailView, name='course_details'),
-
 
 ]
